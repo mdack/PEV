aa8d969b59d5b611d9bb5ba7bc6f934b313aa873
Milly <millydack@gmail.com>
2017-03-13 14:44:52 +0100

    Añadida función 3

--- G09P1/src/Cromosoma.java
+++ G09P1/src/Cromosoma.java
@@ -32,6 +32,25 @@ public abstract class Cromosoma {
 		this.genes = genes;
 	}
 	
+	/**
+	 * Convierte el contenido de la cadena binaria que lleva el gen en su valor decimal
+	 * @param gen Gen que se traducirá a decimal
+	 * @return Valor decimal de la cadena binaria
+	 */
+	public double bin_dec(Gen gen) {
+		int longGen = gen.getLongAlelo();
+		int pos = 0;
+		double valor = 0;
+		
+		for(int i = longGen; i >= 0; i--){
+			double num = (float)Math.pow(2, pos);
+			pos++;
+			valor += num;
+		}
+		
+		return valor;
+	}
+	
 	public abstract double fenotipo(int pos);
 	public abstract double evalua(int pos);
 }
