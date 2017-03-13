3d9182fdf1a130cbcbc8e6fb2179c8ed2dcbdda7
Milly <millydack@gmail.com>
2017-03-13 15:27:23 +0100

    CromosomaP1F3 ok

---
+++ G09P1/src/AGeneticoP1F3.java
@@ -0,0 +1,33 @@
+
+public class AGeneticoP1F3 extends AGenetico {
+
+	public AGeneticoP1F3(int poblacion, int generaciones, float porcCruces, float porcMutacion, float precision) {
+		super(poblacion, generaciones, porcCruces, porcMutacion, precision);
+		// TODO Auto-generated constructor stub
+	}
+
+	@Override
+	public void inicializar() {
+		// TODO Auto-generated method stub
+
+	}
+
+	@Override
+	public void evaluar() {
+		// TODO Auto-generated method stub
+
+	}
+
+	@Override
+	public void seleccion(int tipo) {
+		// TODO Auto-generated method stub
+
+	}
+
+	@Override
+	public void mutacion() {
+		// TODO Auto-generated method stub
+
+	}
+
+}
--- G09P1/src/CromosomaP1F3.java
+++ G09P1/src/CromosomaP1F3.java
@@ -1,16 +1,16 @@
 
 public class CromosomaP1F3 extends Cromosoma {
 	public static final int N_GENES = 2;
-	private static final float X_MIN = -3f;
-	private static final float X_MAX = 12.1f;
-	private static final float Y_MIN = 4.1f;
-	private static final float Y_MAX = 5.8f;
+	private static final double X_MIN = -3;
+	private static final double X_MAX = 12.1;
+	private static final double Y_MIN = 4.1;
+	private static final double Y_MAX = 5.8;
 	
 	public CromosomaP1F3(double tolerancia){
 		genes = new Gen[N_GENES];
 
 		for(int i = 0; i < N_GENES; i++){	
-			int longGen = longitudGen(tolerancia);
+			int longGen = longitudGen(tolerancia, i);
 			this.genes[i] = new Gen(longGen);
 		}
 	}
@@ -20,8 +20,18 @@ public class CromosomaP1F3 extends Cromosoma {
 	 * @param precision Precisión o tolerancia que se utiliza para calcula la longitud del cromosoma
 	 * @return Longitud que tendrá la cadena binaria
 	 */
-	public int longitudGen(double precision){
-		double longAux = (Math.log10((1+((X_MAX - X_MIN) / precision))) / Math.log10(2)); //Calcula la longitud exacta
+	public int longitudGen(double precision, int gen){
+		double longAux = 0;
+		
+		switch(gen){
+		case 0:
+			longAux = (Math.log10((1+((X_MAX - X_MIN) / precision))) / Math.log10(2)); //Calcula la longitud exacta
+			break;
+		case 1:
+			longAux = (Math.log10((1+((Y_MAX - Y_MIN) / precision))) / Math.log10(2)); //Calcula la longitud exacta
+			break;
+		}
+
 		int p_ent = (int) longAux; //Nos quedamos con la parte entera
 		double p_dec = longAux - p_ent; //Obtenemos la parte decimal
 		
@@ -31,14 +41,41 @@ public class CromosomaP1F3 extends Cromosoma {
 	}
 	
 	public double fenotipo(int pos) {
-		// TODO Auto-generated method stub
-		return 0;
+		double valor;
+		fenotipo = 0;
+		Gen gen = this.genes[pos];
+		
+		switch(pos){
+		case 0:
+			valor = X_MIN + ((X_MAX - X_MIN) * bin_dec(gen)) / (Math.pow(2, gen.getLongAlelo()) - 1);
+			fenotipo += valor;
+			break;
+		case 1:
+			valor = Y_MIN + ((Y_MAX - Y_MIN) * bin_dec(gen)) / (Math.pow(2, gen.getLongAlelo()) - 1);
+			fenotipo += valor;
+			break;
+		}
+
+		return fenotipo;
 	}
 
 	@Override
 	public double evalua(int pos) {
-		// TODO Auto-generated method stub
-		return 0;
+		double x = 0;
+		double y = 0;
+		
+		switch(pos){
+		case 0:
+			x = fenotipo(pos);
+			break;
+		case 1:
+			y = fenotipo(pos);
+			break;
+		}
+		
+		double fit = 21.5 + x * Math.sin((4*Math.PI*x)) + y * Math.sin((20*Math.PI*x));		
+		
+		return fit;
 	}
 
 }
