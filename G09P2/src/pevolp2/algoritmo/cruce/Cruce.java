6855ba7bf274d7ea30fb9a6720447e8624beb674
Milly <millydack@gmail.com>
2017-04-03 15:23:25 +0200

    cruce

---
+++ G09P2/src/pevolp2/algoritmo/cruce/Cruce.java
@@ -0,0 +1,13 @@
+package pevolp2.algoritmo.cruce;
+
+import pevolp2.algoritmo.cromosoma.Cromosoma;
+
+public abstract class Cruce {
+
+	public Cruce() {
+		// TODO Auto-generated constructor stub
+	}
+	
+	public abstract void cruzar(Cromosoma padre1, Cromosoma padre2, Cromosoma hijo1, Cromosoma hijo2, int punto_cruce);
+
+}
