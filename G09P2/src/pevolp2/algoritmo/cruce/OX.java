3f8b7b7ca720ce55f61ac3943b894b7829706a16
Milly <millydack@gmail.com>
2017-04-18 21:25:41 +0200

    Cruce OX funciona

--- G09P2/src/pevolp2/algoritmo/cruce/OX.java
+++ G09P2/src/pevolp2/algoritmo/cruce/OX.java
@@ -7,8 +7,8 @@ import pevolp2.algoritmo.cromosoma.Cromosoma;
 
 public class OX extends Cruce {
 	
-	private int puntoA;
-	private int puntoB;
+	private static int puntoA;
+	private static int puntoB;
 
 	public OX() {
 		puntoA = 0;
@@ -19,79 +19,103 @@ public class OX extends Cruce {
 	public void cruzar(Cromosoma padre1, Cromosoma padre2, Cromosoma hijo1, Cromosoma hijo2) {
 		generaPuntos(padre1.getNGenes());	
 		
-		ArrayList<Integer> p1 = new ArrayList<Integer>(padre1.getNGenes());
+		int[] p1 = new int[padre1.getNGenes()];
+		int[] p2 = new int[padre1.getNGenes()];
+		
 		llenaArray(p1, padre2);
-		ArrayList<Integer> p2 = new ArrayList<Integer>(padre2.getNGenes());
 		llenaArray(p2, padre1);
 		
-		completaArrayFinal(p1, padre1);
-		completaArrayInicio(p1, padre1);
-		completaArrayFinal(p2, padre2);
-		completaArrayInicio(p2, padre2);
+		completaArray(p1, padre1);
+		completaArray(p2, padre2);
 		
 		genesHijo(p1, hijo1);
 		genesHijo(p2, hijo2);
 	}
 
-	private void genesHijo(ArrayList<Integer> p, Cromosoma hijo) {
+	private void genesHijo(int[] p, Cromosoma hijo) {
 		
-		for(int i = 0; i< p.size(); i++){
-			hijo.getGenes()[i].setAlelo(p.get(i));;
+		for(int i = 0; i< p.length; i++){
+			hijo.getGenes()[i].setAlelo(p[i]);;
 		}
 	}
 
-	private void completaArrayInicio(ArrayList<Integer> p, Cromosoma padre) {
-		int k = 0;
-		for(int i = 0; i < puntoB; i++){
-			if(!buscaElemento(p, padre.getGenes()[i].getAlelo())){
-				p.add(k, padre.getGenes()[i].getAlelo());
-				k++;
-			}
-		}
-	}
-
-	private void completaArrayFinal(ArrayList<Integer> p, Cromosoma padre) {
+	private void completaArray(int[] p, Cromosoma padre) {
 		int k = puntoB;
-		for(int i = puntoB; i < padre.getNGenes(); i++){
-			if(!buscaElemento(p, padre.getGenes()[i].getAlelo())){
-				p.add(k, padre.getGenes()[i].getAlelo());
+		boolean enc = false;
+		int elem = 0;
+		int limit = padre.getNGenes();
+		
+		for(int i = puntoB; i < p.length; i++){
+			
+			while(k < limit && !enc){
+				elem = padre.getGenes()[k].getAlelo();
+				if(!buscaElemento(p, elem)) enc = true;
 				k++;
+				if(k == padre.getNGenes()){
+					k = 0;
+					limit = puntoB;
+				}
 			}
+
+			if(enc) p[i] = elem;
+			enc = false;
+		}
+		
+		for(int i = 0; i < puntoA; i++){
+			while(k < limit && !enc){
+				elem = padre.getGenes()[k].getAlelo();
+				if(!buscaElemento(p, elem)) enc = true;
+				k++;
+				if(k == padre.getNGenes()){
+					k = 0;
+					limit = puntoB;
+				}
+			}
+
+			if(enc) p[i] = elem;
+			enc = false;
 		}
 		
 	}
 
-	private boolean buscaElemento(ArrayList<Integer> p, int elem) {
+	private boolean buscaElemento(int[] p, int elem) {
 		boolean enc = false;
 		int i = puntoA;
 	
 		while(i < puntoB && !enc){
-			if(p.get(i) == elem) enc = true;
+			if(p[i] == elem) enc = true;
 			i++;
 		}
 		return enc;
 	}
 
-	private void llenaArray(ArrayList<Integer> p, Cromosoma padre) {
+	private void llenaArray(int[] p, Cromosoma padre) {
 		
 		for(int i = 0; i < padre.getNGenes(); i++){
-			p.add(i, 0);
+			p[i] = 0;
 		}
 		
 		for(int i = puntoA; i < puntoB; i++){
-			p.add(i, padre.getGenes()[i].getAlelo());
+			p[i] = padre.getGenes()[i].getAlelo();
+			
 		}
 	}
 
 	private void generaPuntos(int longitud) {
+		int fin = longitud - 2;
 		Random rnd = new Random();
-		int p1 = longitud - 2;
-		puntoA = rnd.nextInt(p1)+1;
 		
-		do{
-		int aux = rnd.nextInt(longitud-puntoA);
-		puntoB = puntoA + aux;
-		}while(puntoB == puntoA);
+		puntoA = (int) ((rnd.nextDouble() * fin) + 1);
+		
+		fin = longitud - 1;
+		int ini = puntoA + 1;
+		if(fin == ini)
+			puntoB = fin;
+		else{
+			do{
+				puntoB = (int) ((rnd.nextDouble() * fin) + ini);
+			}while(puntoB >= longitud);
+		}
 		
 	}
 	
