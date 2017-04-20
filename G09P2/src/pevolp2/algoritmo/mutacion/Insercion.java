24aa18ce96029c9de99d2be7b43d2a080bf4f638
Jabp95 <ramik199517@gmail.com>
2017-04-20 18:24:21 +0200

    Inversion e insercion implementadas.

--- G09P2/src/pevolp2/algoritmo/mutacion/Insercion.java
+++ G09P2/src/pevolp2/algoritmo/mutacion/Insercion.java
@@ -1,18 +1,53 @@
 package pevolp2.algoritmo.mutacion;
 
+import java.util.Random;
+
 import pevolp2.algoritmo.cromosoma.Cromosoma;
 
 public class Insercion extends Mutacion {
 
-
+	private final double inserciones = 2;
+	
 	public Insercion(double prob) {
 		super(prob);
 	}
 
 	@Override
 	public void mutar(Cromosoma[] pob) {
-		// TODO Auto-generated method stub
-		
+		Random rnd = new Random();
+		for(int i = 0; i < pob.length; i++)
+		{
+			double p = rnd.nextDouble();
+			if(p < prob_mutacion)
+			{
+				Cromosoma c = pob[i];
+				for(int j = 0; j < inserciones; j++)
+				{
+					int posicionACambiar = rnd.nextInt(c.getNGenes());
+					int posicionDondeInsertar = rnd.nextInt(c.getNGenes());
+					while(posicionACambiar == posicionDondeInsertar) posicionDondeInsertar = rnd.nextInt(c.getNGenes());
+					if(posicionACambiar < posicionDondeInsertar)
+					{
+						int aux = c.getGenes()[posicionACambiar].getAlelo();
+						for(int k = posicionACambiar; k < posicionDondeInsertar; k++)
+						{
+							c.getGenes()[k].setAlelo(c.getGenes()[k+1].getAlelo());
+						}
+						c.getGenes()[posicionDondeInsertar].setAlelo(aux);
+					}
+					else if(posicionACambiar > posicionDondeInsertar)
+					{
+						int aux = c.getGenes()[posicionACambiar].getAlelo();
+						for(int k = posicionACambiar; k > posicionDondeInsertar; k--)
+						{
+							c.getGenes()[k].setAlelo(c.getGenes()[k-1].getAlelo());
+						}
+						c.getGenes()[posicionDondeInsertar].setAlelo(aux);
+					}
+					pob[i] = c.copia();
+				}
+			}
+		}
 	}
 
 }
--- G09P2/src/pevolp2/algoritmo/mutacion/Inversion.java
+++ G09P2/src/pevolp2/algoritmo/mutacion/Inversion.java
@@ -1,5 +1,8 @@
 package pevolp2.algoritmo.mutacion;
 
+import java.util.Random;
+import java.util.Stack;
+
 import pevolp2.algoritmo.cromosoma.Cromosoma;
 
 public class Inversion extends Mutacion {
@@ -7,13 +10,32 @@ public class Inversion extends Mutacion {
 
 	public Inversion(double prob) {
 		super(prob);
-		// TODO Auto-generated constructor stub
 	}
 
 	@Override
 	public void mutar(Cromosoma[] pob) {
-		// TODO Auto-generated method stub
-		
+		Random rnd = new Random();
+		for(int i = 0; i < pob.length; i++)
+		{
+			double p = rnd.nextDouble();
+			if(p < prob_mutacion)
+			{
+				Cromosoma c = pob[i];
+				int puntoA = rnd.nextInt(c.getLongitud());
+				int puntoB = rnd.nextInt(c.getLongitud() - puntoA);
+				puntoB += puntoA;
+				Stack<Integer> s = new Stack<Integer>();
+				for(int j = puntoA; j < puntoB; j++)
+				{
+					s.push(c.getGenes()[j].getAlelo());
+				}
+				for(int j = puntoA; j < puntoB; j++)
+				{
+					c.getGenes()[j].setAlelo(s.pop());
+				}
+				pob[i] = c.copia();
+			}
+		}
 	}
 
 }
